08a59974c536660fcf95a0175214da62cbdb40cb
dz1237 <88466558+dz1237@users.noreply.github.com>
2021-12-21 16:16:34 +0800

    update 博客

---
+++ 博客/demo2/src/Footer.jsx
@@ -0,0 +1,11 @@
+import { render } from "less";
+import React, { Component } from "react";
+export default class Footer extends Component {
+    render() {
+        return (
+            <div className="footer">
+                <p>&copy; CopyRight 2018 XX公司版权所有</p>
+            </div>
+        )
+    }
+}
