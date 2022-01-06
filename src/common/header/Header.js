2d84ff836bfb75a600b31ff31e0bc08f4e8c556d
dz1237 <88466558+dz1237@users.noreply.github.com>
2022-01-06 19:52:48 +0800

    update 搜索框动画

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { connect } from 'react-redux';
 import { CSSTransition } from 'react-transition-group';
 import {
@@ -11,62 +11,61 @@ import {
     Button,
     SearchWrapper
 } from './style'
+
+const Header = (props) => {
+
+    let { focused, handleFocus, handleBlur } = props;
+    return (
+        <HeaderWrapper >
+            <Logo href='/' />
+            <Nav>
+                <NavItem className='left active'>首页</NavItem>
+                <NavItem className='left'>下载App</NavItem>
+                <NavItem className='right'>登录</NavItem>
+                <NavItem className='right'>
+                    <span className="iconfont">&#xe600;</span>
+                </NavItem>
+                <SearchWrapper>
+                    <CSSTransition timeout={500} in={focused} classNames="slide">
+                        <NavSearch className={focused ? "focused" : ""}
+                            onFocus={handleFocus}
+                            onBlur={handleBlur}
+                        ></NavSearch>
+                    </CSSTransition>
+                    <span className={focused ? "focused iconfont" : "iconfont"}>&#xe617;</span>
+                </SearchWrapper>
+            </Nav>
+            <Addition>
+                <Button className='writing'>
+                    <span className="iconfont">&#xe615;</span>
+                    写文章
+                </Button>
+                <Button className='reg'>注册</Button>
+            </Addition>
+        </HeaderWrapper >
+    )
+}
+
 const mapStateToProps = (state) => {
     return {
-
+        focused: state.focused
     }
 }
 const mapDispatchToProsp = (dispatch) => {
     return {
+        handleFocus() {
+            const action = {
+                type: 'ADD'
+            }
+            dispatch(action);
 
+        },
+        handleBlur() {
+            const action = {
+                type: 'SUB'
+            }
+            dispatch(action);
+        }
     }
 }
-@connect(mapStateToProps, mapDispatchToProsp)
-class Header extends Component {
-    state = {
-        focused: false
-    }
-    handleFocus = () => {
-        this.setState({
-            focused: true
-        })
-    }
-    handleBlur = () => {
-        this.setState({
-            focused: false
-        })
-    }
-    render() {
-        let { focused } = this.state;
-        return (
-            <HeaderWrapper >
-                <Logo href='/' />
-                <Nav>
-                    <NavItem className='left active'>首页</NavItem>
-                    <NavItem className='left'>下载App</NavItem>
-                    <NavItem className='right'>登录</NavItem>
-                    <NavItem className='right'>
-                        <span className="iconfont">&#xe600;</span>
-                    </NavItem>
-                    <SearchWrapper>
-                        <CSSTransition timeout={500} in={focused} classNames="slide">
-                            <NavSearch className={this.state.focused ? "focused" : ""}
-                                onFocus={this.handleFocus}
-                                onBlur={this.handleBlur}
-                            ></NavSearch>
-                        </CSSTransition>
-                        <span className={this.state.focused ? "focused iconfont" : "iconfont"}>&#xe617;</span>
-                    </SearchWrapper>
-                </Nav>
-                <Addition>
-                    <Button className='writing'>
-                        <span className="iconfont">&#xe615;</span>
-                        写文章
-                    </Button>
-                    <Button className='reg'>注册</Button>
-                </Addition>
-            </HeaderWrapper >
-        )
-    }
-}
-export default Header;
+export default connect(mapStateToProps, mapDispatchToProsp)(Header);
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,6 +1,16 @@
-const defaultState = {};
+const defaultState = {
+    focused: false
+};
 const reducer = (state = defaultState, action) => {
     switch (action.type) {
+        case 'ADD':
+            return {
+                focused: true
+            }
+        case 'SUB':
+            return {
+                focused: false
+            }
         default:
             return state;
     }
